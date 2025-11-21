becf8876074f07be4a4d9ea5805429d4589f1389
pecezon <lopez.diego@cetys.edu.mx>
2025-11-20 16:50:46 -0800

    Landing Page with integrated login

---
+++ frontend/src/pages/Landing.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button } from "@heroui/react";
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useAuth } from "../auth/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function Landing() {
+  const { user, loading, loginWithGoogle } = useAuth();
+
+  if (loading) return null;
+
+  if (user) return <Navigate to="/home" />;
+
+  return (
+    <div className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[url('/images/playa-del-carmen.avif')] md:bg-[url('/images/riviera-maya.jpg')]">
+      <div
+        className="w-full h-screen bg-cover bg-center bg-no-repeat flex pt-8 items-center flex-col gap-4 text-center text-white md:pt-16"
+        style={{ backgroundImage: "url('/images/wavy-top.svg')" }}
+      >
+        <h1 className="text-4xl font-bold">Welcome to PilloVan</h1>
+        <h3>“Tu aventura empieza con nosotros.”</h3>
+      </div>
+      <div className="absolute inset-0 flex items-center justify-center">
+        <Button
+          color="primary"
+          endContent={<i className="fa-solid fa-bus-simple" />}
+          onPress={loginWithGoogle}
+        >
+          Comienza tu viaje
+        </Button>
+      </div>
+    </div>
+  );
+}
