6aba256c12eb997b6aefb98fca091a6546adeaa3
pecezon <lopez.diego@cetys.edu.mx>
2025-11-23 00:47:02 -0800

    more react-query

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,30 +4,39 @@ import Landing from "./pages/Landing";
 import { Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import Homepage from "./pages/Homepage";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Landing />} />
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="/" element={<Landing />} />
 
-      <Route
-        path="/onboarding"
-        element={
-          <ProtectedRoute>
-            <Onboarding />
-          </ProtectedRoute>
-        }
-      />
+        <Route
+          path="/onboarding"
+          element={
+            <ProtectedRoute>
+              <Onboarding />
+            </ProtectedRoute>
+          }
+        />
 
-      <Route
-        path="/home"
-        element={
-          <ProtectedRoute>
-            <Homepage />
-          </ProtectedRoute>
-        }
-      />
-    </Routes>
+        <Route
+          path="/home"
+          element={
+            <ProtectedRoute>
+              <Homepage />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </QueryClientProvider>
   );
 }
 
